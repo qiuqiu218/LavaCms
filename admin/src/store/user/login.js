b4df78ea6ed41372ae5ec98a583d12b7ced5f96b
万鑫 <295686243@qq.com>
2019-01-08 18:01:06 +0800

    登录模块

---
+++ admin/src/store/user/login.js
@@ -0,0 +1,23 @@
+const state = {
+  username: '',
+  password: ''
+}
+
+const mutations = {
+  LOGIN (state) {
+
+  }
+}
+
+const actions = {
+  login () {
+
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -4,10 +4,13 @@
       <p slot="title">管理员登录</p>
       <Form label-position="right" :label-width="50">
         <FormItem label="用户名">
-          <Input placeholder="请输入用户名"></Input>
+          <Input placeholder="请输入用户名" v-model="data.username"></Input>
         </FormItem>
         <FormItem label="密码">
-          <Input placeholder="请输入密码"></Input>
+          <Input placeholder="请输入密码" v-model="data.password"></Input>
+        </FormItem>
+        <FormItem>
+          <Button type="primary" @click="submit">登录</Button>
         </FormItem>
       </Form>
     </Card>
@@ -17,7 +20,20 @@
 <script>
 export default {
   data () {
-    return {}
+    return {
+      data: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    submit () {
+      this.get('login', this.data)
+        .then(res => {
+          console.log(res)
+        })
+    }
   }
 }
 </script>
