660fbf0f88547a4c1f9cba88accda7f2f4330ad8
万鑫 <295686243@qq.com>
2019-01-13 11:51:52 +0800

    新增font(文字)组件

---
+++ admin/src/components/Font.vue
@@ -0,0 +1,21 @@
+<template>
+  <span :class="[fontSize]">
+    <slot></slot>
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    size: {
+      type: Number,
+      default: 10
+    }
+  },
+  computed: {
+    fontSize () {
+      return 'w-font-' + this.size
+    }
+  }
+}
+</script>
