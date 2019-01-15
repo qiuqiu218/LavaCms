66a4a4d17be01bd4d06d3fc6afb6a50b19291418
万鑫 <295686243@qq.com>
2019-01-15 18:10:49 +0800

    添加menu模块的 Observer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Admin\Menu;
+use App\Observers\Admin\MenuObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Menu::observe(MenuObserver::class);
     }
 
     /**
