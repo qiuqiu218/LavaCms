7cc87fd10fdd02a7522926f09ba10b9d08d786b0
万鑫 <295686243@qq.com>
2019-01-15 18:08:02 +0800

    增加 common/menu 模块缓存

--- admin/src/plugins/cache.js
+++ admin/src/plugins/cache.js
@@ -1,5 +1,7 @@
 import store from 'store2'
 
 export default {
-  user: store.namespace('user')
+  user: store.namespace('user'),
+  common: store.namespace('common'),
+  menu: store.namespace('menu')
 }
